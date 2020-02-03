0c423f0765cee498dd126722d15df64d191e4408
Richard Williams <richard.lluttrell@gmail.com>
2020-02-03 07:26:41 -0700

    added function to prompt user for new dimensions and reset the board

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const gameContainer = document.getElementById("game-container");
 const resetButton = document.getElementById("reset-button");
 
 drawBoard(16); // initial board set up
+resetButton.addEventListener('click', promptUserAndReset);
 
 function drawBoard(gridWidth) {
     for (let i = 0; i < gridWidth*gridWidth; i++) {
@@ -10,7 +11,24 @@ function drawBoard(gridWidth) {
         let squareSize = 600/gridWidth + "px"
         li.style.width = squareSize;
         li.style.height = squareSize;
-        li.addEventListener('mouseover', () => { li.style.backgroundColor = "#AAAAAA"})
+        li.addEventListener('mouseover', () => { li.style.backgroundColor = "#444444"})
         gameContainer.appendChild(li);
+    }
+}
+
+function resetBoard(gridWidth) {
+    // remove existing elements
+    while (gameContainer.firstChild) {
+        gameContainer.removeChild(gameContainer.firstChild);
+    }
+    drawBoard(gridWidth);
+}
+
+function promptUserAndReset() {
+    let newWidth = prompt("Enter a new width");
+    if (newWidth >= 2) {
+        resetBoard(newWidth);
+    } else {
+        resetBoard(16);
     }
 }
